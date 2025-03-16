3be7d85d68794b6ac2c8a9fb0d4a36bbcf7e414e
Anrich Boer <anrich.boer637@gmail.com>
2025-03-16 15:32:32 +0200

    Second push Sunday

--- api/UsersControllerApi/obj/Debug/net8.0/UsersController.AssemblyInfo.cs
+++ api/UsersControllerApi/obj/Debug/net8.0/UsersController.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469542fd7e8df27fff6a5382abda29eb7ad9cd6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f459308d6e99be3eea01390df4bca8d7853623aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
